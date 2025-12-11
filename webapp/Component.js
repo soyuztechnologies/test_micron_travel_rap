e6fabde25df9d1ce85faf06d053e811a77b9456e
Anubhav Oberoi <contact@anubhavtrainings.com>
2025-12-11 04:20:50 +0000

    my first stable app

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("mic.ab.managetravel.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
